5821ebdd709f010e6465f5cc942dee89cb9142da
lbair39 <lilybair24@g.crookcountyschools.org>
2021-10-22 09:48:46 -0700

    I finished my JS

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 console.log('My script is working');
 document.write('<h1>This is my Intro to JavaScript Project</h1>')
 
+var name = "Lily Bair";
 var numb = 22;
 var numb2 = 34;
+
+console.log(name)
 console.log(numb + numb2);
+
+function introduction(name, age){
+    console.log('Hello my name is ' + name + ' and I am ' + age +' years old.')
+}
+introduction("Lily", "16")
+alert('this is an alert')
+
+prompt('how are you today?')
